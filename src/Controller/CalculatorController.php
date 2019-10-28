9bd039d8a7080e4a9b66d425a93f5e3a09b302df
Tom Butts <tom.butts93@gmail.com>
2019-10-28 10:35:38 +0000

    Basic functionality

--- src/Controller/CalculatorController.php
+++ src/Controller/CalculatorController.php
@@ -2,9 +2,15 @@
 
 namespace App\Controller;
 
+use Exception;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Psr\Log\LoggerInterface;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Session\Session;
+
 use App\Service\Calculator;
 
 /**
@@ -12,49 +18,102 @@ use App\Service\Calculator;
  */
 class CalculatorController extends AbstractController
 {
+    protected $calculator;
+
+    protected $calculatorEntity;
+
+    protected $logger;
+
+    public function __construct(Calculator $calculator, LoggerInterface $logger)
+    {
+        $this->calculator = $calculator;
+        $this->logger = $logger;
+
+        try {
+            $this->calculatorEntity = $this->calculator->persist();
+        } catch (Exception $e) {
+            $this->logger->critical($e->getMessage());
+
+            // TODO: Redirect to an error page.
+        }
+    }
+
     /**
      * @Route("/", name="index")
+     * @return Response
+     * @throws Exception
      */
-    public function index(Calculator $calculator)
+    public function index()
     {
-        $calculator->persist();
-
-        return $this->render('calculator.html.twig', [ 'current' => '' ]);
+        return $this->render('calculator.html.twig', [
+            'current' => $this->calculatorEntity->getCurrent()
+        ]);
     }
 
     /**
      * @Route("/append/{value}", name="append")
+     * @param $value
+     * @return
      */
     public function append($value)
     {
-        echo '<pre>';
-        print_r('hi');
-        echo '</pre>';
-        die();
+        $this->calculator->append($value);
+
+        return $this->redirectToRoute('calculator_index');
     }
 
     /**
      * @Route("/operand/{value}", name="operand")
+     * @param $value
+     * @return RedirectResponse
      */
     public function operand($value)
     {
+        if (
+            $this->calculatorEntity->getCurrent() &&
+            $this->calculatorEntity->getPrevious() &&
+            !$this->calculatorEntity->getOperandActive()
+        ) {
+            // If its a next operation just redirect to equals
+            return $this->redirectToRoute('calculator_equal', [ 'nextOperation' => $value ]);
+        }
 
+        // Handle updating all values in the case of a new operand
+        $this->calculator->setOperand($value);
+
+        return $this->redirectToRoute('calculator_index');
     }
 
     /**
      * @Route("/updateCurrent/{operation}", name="updateCurrent")
+     * @param $operation
+     * @return RedirectResponse
      */
     public function updateCurrent($operation)
     {
+        try {
+            $this->calculator->updateCurrent($operation);
+        } catch (Exception $e) {
+            $this->logger->critical($e->getMessage(), [ 'operation' => $operation ]);
+        }
 
+        return $this->redirectToRoute('calculator_index');
     }
 
     /**
      * @Route("/equal", name="equal")
+     * @param string $nextOperation
+     * @return RedirectResponse
      */
-    public function equal()
+    public function equal($nextOperation = '')
     {
+        try {
+            $this->calculator->equal($nextOperation);
+        } catch (Exception $e) {
+            $this->logger->critical($e->getMessage());
+        }
 
+        return $this->redirectToRoute('calculator_index');
     }
 
     /**
@@ -62,7 +121,9 @@ class CalculatorController extends AbstractController
      */
     public function clear()
     {
+        $this->calculator->clear();
 
+        return $this->redirectToRoute('calculator_index');
     }
 
     /**
--- src/Service/Calculator.php
+++ src/Service/Calculator.php
@@ -2,38 +2,172 @@
 
 namespace App\Service;
 
-use Symfony\Component\HttpFoundation\Session\Session;
+use Exception;
+
+use App\Entity\Calculator as CalculatorEntity;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class Calculator
 {
-    private $session;
+    protected $calculatorEntity;
 
-    public function __construct()
+    protected $entityManager;
+
+    protected $session;
+
+    public function __construct(EntityManagerInterface $entityManager, SessionInterface $session)
     {
-        $this->session = new Session();        
+        $this->entityManager = $entityManager;
+        $this->session = $session;
+
+        $this->persist();
     }
 
     public function persist()
     {
-        $this->session->set('calculator', 'hi');
-        dump($this->session->geta('calculator'));
+        $calculatorId = $this->session->get('calculator');
 
-        // Check session for calc
-        // if not add db row and put into sesh
-        // else just return the calculator instance
+        if (!$calculatorId) {
+            // Persist calculator data
+            $calculator = new CalculatorEntity();
+            $this->entityManager->persist($calculator);
+            $this->entityManager->flush();
 
-        // if (!$request->session()->has('calculator')) {
-        //     $calculator = Calculator::create([
-        //         'current' => '',
-        //         'previous' => '',
-        //         'operand' => '',
-        //         'memory' => ''
-        //     ]);
-        //     $request->session()->put('calculator', $calculator->id);
-        // }
-        // $calculator = Calculator::find($request->session()->get('calculator'));
-        // if (!$calculator) {
-        //     $request->session()->forget('calculator');
-        // }
+            $calculatorId = $calculator->getId();
+
+            // Set calculator id in session
+            $this->session->set('calculator', $calculatorId);
+        }
+
+        // Use session id to get calculator instance
+        $calculator = $this->entityManager
+            ->getRepository(CalculatorEntity::class)
+            ->find($calculatorId);
+
+        if (!$calculator) {
+            // Reset session to fetch new calculator next request
+            $this->session->set('calculator', '');
+
+            throw new Exception('Calculator not found.');
+        }
+
+        $this->calculatorEntity = $calculator;
+
+        return $calculator;
+    }
+
+    public function equal($nextOperation = '')
+    {
+        $current = $this->calculatorEntity->getCurrent();
+        $previous = $this->calculatorEntity->getPrevious();
+
+        switch ($this->calculatorEntity->getOperand()) {
+            case 'add':
+                $current = (float) $previous + (float) $current;
+                break;
+            case 'minus':
+                $current = (float) $previous - (float) $current;
+                break;
+            case 'multiply':
+                $current = (float) $previous * (float) $current;
+                break;
+            case 'divide':
+                if (!$previous || !$current) {
+                    $current = 0;
+                }
+                $current = (float) $previous / (float) $current;
+                break;
+            default:
+                throw new Exception('Operation not recognised in equals.');
+                break;
+        }
+
+        $this->calculatorEntity->setCurrent($current);
+        $this->calculatorEntity->setPrevious(($nextOperation !== '') ? $current : '');
+        $this->calculatorEntity->setOperand($nextOperation);
+        $this->calculatorEntity->setOperandActive(($nextOperation !== '') ? true : false);
+
+        $this->entityManager->persist($this->calculatorEntity);
+        $this->entityManager->flush();
+
+        return $this->calculatorEntity;
+    }
+
+    /**
+     * Handle appending a value to the calculator
+     *
+     * @param $value
+     */
+    public function append($value)
+    {
+        $current = $this->calculatorEntity->getCurrent();
+
+        $this->calculatorEntity->setCurrent($current . $value);
+
+        if ($this->calculatorEntity->getOperandActive()) {
+            $this->calculatorEntity->setOperandActive(false);
+            $this->calculatorEntity->setCurrent($value);
+        }
+
+        $this->entityManager->persist($this->calculatorEntity);
+        $this->entityManager->flush();
+    }
+
+    public function setOperand($value)
+    {
+        $this->calculatorEntity->setPrevious($this->calculatorEntity->getCurrent());
+        $this->calculatorEntity->setOperand($value);
+        $this->calculatorEntity->setOperandActive(true);
+
+        $this->entityManager->persist($this->calculatorEntity);
+        $this->entityManager->flush();
+
+        return $this->calculatorEntity;
+    }
+
+    public function updateCurrent($operation)
+    {
+        $current = $this->calculatorEntity->getCurrent();
+
+        switch ($operation) {
+            case 'percent':
+                $current = (float) $current / 100;
+                break;
+            case 'sign':
+                if (substr($current, 0, 1) === '-') {
+                    $current = ltrim($current, '-');
+                } else {
+                    $current = '-' . $current;
+                }
+                break;
+            case 'point':
+                if (strpos($current, '.') === false) {
+                    $current .= '.';
+                }
+                break;
+
+            default:
+                throw new Exception('Operation name not recognised!');
+                break;
+        }
+
+        $this->calculatorEntity->setCurrent($current);
+
+        $this->entityManager->persist($this->calculatorEntity);
+        $this->entityManager->flush();
+
+        return $this->calculatorEntity;
+    }
+
+    public function clear()
+    {
+        $this->calculatorEntity->setCurrent('');
+        $this->calculatorEntity->setPrevious('');
+        $this->calculatorEntity->setOperand('');
+        $this->calculatorEntity->setOperandActive(false);
+
+        $this->entityManager->persist($this->calculatorEntity);
+        $this->entityManager->flush();
     }
 }
